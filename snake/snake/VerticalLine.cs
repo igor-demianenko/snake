05e245badf1e873b0981d9ea2904ec95c84ac45e
Igor D <igor.demianenko@gail.com>
2018-04-03 21:35:01 +0300

    fix DZ

--- snake/snake/VerticalLine.cs
+++ snake/snake/VerticalLine.cs
@@ -13,9 +13,9 @@ namespace snake
         public VerticalLine(int yUp, int yDown, int x, char sym)
         {
             pList = new List<Point>();
-            for (int y = yUp; y <= yDown; x++)
+            for (int y = yUp; y <= yDown; y++)
             {
-                Point p = new Point(y, x, sym);
+                Point p = new Point(x, y, sym);
                 pList.Add(p);
             }
 
